0510ace0d25734830cc59bd4dd6c377bc08c9e8e
jacecao <280749498@qq.com>
2018-03-09 18:02:17 +0800

    sticky-footer ?

--- src/components/header/detail/detail.vue
+++ src/components/header/detail/detail.vue
@@ -1,30 +1,38 @@
 <template>
   <div class="header-detail">
-    <div class="detail-title">
-      <h3>{{seller.name}}</h3>
-      <v-star :size="36" :score="seller.score"/>
-    </div>
-    <!-- 优惠信息 -->
-    <div class="info-content benefit-info">
-      <div class="info-title">
-        <i class="line"></i><span>优惠信息</span><i class="line"></i>
+    <div class="detail-content clearfix">
+      <div class="detail-main">
+        <!--
+        <div class="detail-title">
+          <h3>{{seller.name}}</h3>
+          <v-star :size="36" :score="seller.score"/>
+        </div>
+        -->
+        <!-- 优惠信息 -->
+        <!--
+        <div class="info-content">
+          <div class="info-title">
+            <i class="line"></i><span>优惠信息</span><i class="line"></i>
+          </div>
+          <ul class="benefit-list">
+            <li v-for="supportInfo in (seller.supports)">
+              <i class="icon" :class="classarr[supportInfo.type]"></i><span class="info">{{supportInfo.description}}</span>
+            </li>
+          </ul>
+        </div>
+        -->
+        <!-- 商家公告 -->
+        <!--
+        <div class="info-content">
+          <div class="info-title">
+            <i class="line"></i><span>商家公告</span><i class="line"></i>
+          </div>
+          <p class="bulletin-text">{{}}</p>
+        </div>
+        -->
       </div>
-      <ul class="benefit-list">
-        <li v-for="supportInfo in (seller.supports)">
-          <i class="icon" :class="classarr[supportInfo.type]"></i><span class="info">{{supportInfo.description}}</span>
-        </li>
-      </ul>
     </div>
-
-    <!-- 商家公告 -->
-    <div class="info-content benefit-info">
-      <div class="info-title">
-        <i class="line"></i><span>商家公告</span><i class="line"></i>
-      </div>
-      <p class="bulletin">{{seller.bulletin}}</p>
-    </div>
-
-    <button type="button" class="closed" v-on:click="changStatus"><i class="icon-close"></i></button>
+    <button type="button" class="detail-closed" v-on:click="changStatus"><i class="icon-close"></i></button>
   </div>
 </template>
 
@@ -36,7 +44,7 @@ export default {
     'classarr': Array
   },
   created () {
-    console.log(this.classarr);
+    // console.log(this.classarr);
   },
   methods: {
     changStatus () {
@@ -54,17 +62,21 @@ export default {
 <style scoped>
   @import './detail.css';
 
-  .closed {
+  .detail-closed {
+    position: relative;
     height: 40px;
     width: 40px;
     border: none;
     border-radius: 50%;
     background: transparent;
     box-shadow: 0 0 1px 2px rgba(255,255,255,.2);
+    margin: -64px auto 0 auto;
+    padding: 0;
+    clear: both;
   }
-  .closed i {
-    font-size: 3.2rem;
-    color: rgba(255,255,255,.5);
-  }
+      .detail-closed i {
+        font-size: 3.2rem;
+        color: rgba(255,255,255,.5);
+      }
 
 </style>
